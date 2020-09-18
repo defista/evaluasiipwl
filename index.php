a15ae4ae080550e3167558a687e6309e6aa01ac1
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-18 21:20:47 +0700

    implement connection from koneksi.file

--- index.php
+++ index.php
@@ -1,66 +1,51 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['form_name']) && $_POST['form_name'] == 'loginform')
-{
-   $success_page = './Home-Page.php';
-   $error_page = './Login-Error.html';
-   $mysql_server = 'localhost';
-   $mysql_username = 'root';
-   $mysql_password = '';
-   $mysql_database = 'eipwl';
-   $mysql_table = 'users';
-   $crypt_pass = md5($_POST['password']);
-   $found = false;
-   $fullname = '';
-   $role = '';
-   $avatar = '';
-   $session_timeout = 600;
-   $db = mysqli_connect($mysql_server, $mysql_username, $mysql_password);
-   if (!$db)
-   {
-      die('Failed to connect to database server!<br>'.mysqli_error($db));
-   }
-   mysqli_select_db($db, $mysql_database) or die('Failed to select database<br>'.mysqli_error($db));
-   mysqli_set_charset($db, 'utf8');
-   $sql = "SELECT * FROM ".$mysql_table." WHERE username = '".mysqli_real_escape_string($db, $_POST['username'])."'";
-   $result = mysqli_query($db, $sql);
-   if ($data = mysqli_fetch_array($result))
-   {
-      if ($crypt_pass == $data['password'] && $data['active'] != 0)
-      {
-         $found = true;
-         $fullname = $data['fullname'];
-         $role = $data['role'];
-         $folder = substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/') + 1);
-         $avatar = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$folder" . "avatars/" . $data['avatar'];
-      }
-   }
-   mysqli_close($db);
-   if($found == false)
-   {
-      header('Location: '.$error_page);
-      exit;
-   }
-   else
-   {
-      if (session_id() == "")
-      {
-         session_start();
-      }
-      $_SESSION['username'] = $_POST['username'];
-      $_SESSION['fullname'] = $fullname;
-      $_SESSION['role'] = $role;
-      $_SESSION['avatar'] = $avatar;
-      $_SESSION['expires_by'] = time() + $session_timeout;
-      $_SESSION['expires_timeout'] = $session_timeout;
-      $rememberme = isset($_POST['rememberme']) ? true : false;
-      if ($rememberme)
-      {
-         setcookie('username', $_POST['username'], time() + 3600*24*30);
-         setcookie('password', $_POST['password'], time() + 3600*24*30);
-      }
-      header('Location: '.$success_page);
-      exit;
-   }
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['form_name']) && $_POST['form_name'] == 'loginform') {
+    $success_page = './Home-Page.php';
+    $error_page = './Login-Error.html';
+    include('koneksi.php');
+
+    $mysql_table = 'users';
+    $crypt_pass = md5($_POST['password']);
+    $found = false;
+    $fullname = '';
+    $role = '';
+    $avatar = '';
+    $session_timeout = 600;
+  
+    mysqli_set_charset($db, 'utf8');
+    $sql = "SELECT * FROM ".$mysql_table." WHERE username = '".mysqli_real_escape_string($db, $_POST['username'])."'";
+    $result = mysqli_query($db, $sql);
+    if ($data = mysqli_fetch_array($result)) {
+        if ($crypt_pass == $data['password'] && $data['active'] != 0) {
+            $found = true;
+            $fullname = $data['fullname'];
+            $role = $data['role'];
+            $folder = substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/') + 1);
+            $avatar = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$folder" . "avatars/" . $data['avatar'];
+        }
+    }
+    mysqli_close($db);
+    if ($found == false) {
+        header('Location: '.$error_page);
+        exit;
+    } else {
+        if (session_id() == "") {
+            session_start();
+        }
+        $_SESSION['username'] = $_POST['username'];
+        $_SESSION['fullname'] = $fullname;
+        $_SESSION['role'] = $role;
+        $_SESSION['avatar'] = $avatar;
+        $_SESSION['expires_by'] = time() + $session_timeout;
+        $_SESSION['expires_timeout'] = $session_timeout;
+        $rememberme = isset($_POST['rememberme']) ? true : false;
+        if ($rememberme) {
+            setcookie('username', $_POST['username'], time() + 3600*24*30);
+            setcookie('password', $_POST['password'], time() + 3600*24*30);
+        }
+        header('Location: '.$success_page);
+        exit;
+    }
 }
 $username = isset($_COOKIE['username']) ? $_COOKIE['username'] : '';
 $password = isset($_COOKIE['password']) ? $_COOKIE['password'] : '';
