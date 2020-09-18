8201386d10f46975811ae82c278cdc289f7b29de
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-18 21:20:02 +0700

    add connection configuration

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host       = "localhost";
+$user       = "root";
+$password   = "root";
+$database   = "eipwl";
+$port       = 3306;
+$db      = mysqli_connect($host, $user, $password, $database, $port);
